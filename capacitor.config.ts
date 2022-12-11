999db78bbf0dc83dc18b50a9983cf1926e7a3633
colorthoro <dream.0112@qq.com>
2022-12-11 23:22:56 +0800

    build1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,10 +5,6 @@ const config: CapacitorConfig = {
   appName: 'magic_music_ionic',
   webDir: 'dist',
   bundledWebRuntime: false,
-  server: {
-    "url": "http://192.168.43.201:8100/",
-    cleartext: true,
-  }
 };
 
 export default config;
